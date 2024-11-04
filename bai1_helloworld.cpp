ce9154d8e619c4e1d5bc99aea1ec0eddd1025c20
anhdatsiudeptrai <ddat5441@gmail.com>
2024-11-04 11:24:11 +0700

    Add project files.

---
+++ bai1_helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+using namespace std;
+int main() {
+	cout << "hello world!\n";
+	cout << "toi ten la " << "\tle do tien dat" << endl;
+	cout << "toi dang hoc \tc++ " << endl;
+	return 0;
+}
